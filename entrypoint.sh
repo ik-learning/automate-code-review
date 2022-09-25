74184b757f7120ac7f721a53b06da6e950ee2954
ivank <ivankatliarchuk@hollandandbarrett.com>
2022-09-25 11:17:38 +0100

    simplify script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
-set -x
+set -e
 
 : "$WORK_DIR"
 : "$DANGER_GITLAB_HOST"
 
-echo "$PWD"
 export CURRENT_FOLDER="$PWD"
-
 cd "$WORK_DIR"
-
-echo $PWD
 yarn danger ci --dangerfile $CURRENT_FOLDER/dangerfile.js --removePreviousComments
