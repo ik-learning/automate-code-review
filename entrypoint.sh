cd653e281688ce257b1815346c2fd3f22c092951
ivank <ivankatliarchuk@hollandandbarrett.com>
2022-10-19 15:53:13 +0100

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,10 @@ set -e
 : "$WORK_DIR"
 : "$DANGER_GITLAB_HOST"
 
-export CURRENT_FOLDER="$PWD"
+# export CURRENT_FOLDER="$PWD"
 cd "$WORK_DIR"
 echo "VERSION: $VERSION"
 yarn danger --version
-yarn danger ci --dangerfile $CURRENT_FOLDER/dangerfile.js --removePreviousComments
+# TODO: support cases when Danger file is required per -repository
+# yarn danger ci --dangerfile $CURRENT_FOLDER/dangerfile.js --removePreviousComments
+yarn danger ci --dangerfile dangerfile.js --removePreviousComments
