0cb5c5f845232e887f51181c77db2cff941a9b98
ivank <ivankatliarchuk@hollandandbarrett.com>
2022-10-19 19:38:30 +0100

    check teamplates

--- dangerfile.js
+++ dangerfile.js
@@ -1,21 +1,25 @@
 'use strict';
 // Example dangerfile. Every repo could contain this file in order to benefit from automated MR review
-const { danger, warn, message, markdow } = require('danger');
+const { danger, message, warn, fail, markdow } = require('danger');
 const { commonChecks, infraChecks, csvEntryAlphabeticOrderAsync, addLabels, welcomeMsg } = require(
   process.env.IS_CI ? "/danger/lib/dangerfile.paas" : "./lib/dangerfile.paas"
 );
 
 // console.log(danger.gitlab.mr);
-// (async function () {
-//   commonChecks();
-//   await infraChecks();
-// })();
 
-if (danger.gitlab.metadata.repoSlug.includes('msk-topics')) {
+if (danger.gitlab.metadata.repoSlug.includes('platform-as-a-service/kafka/msk-topics')) {
   console.log(`MR "${danger.gitlab.mr.web_url}" review..`);
   (async function () {
     commonChecks();
     await csvEntryAlphabeticOrderAsync();
+  })();
+}
+
+if (danger.gitlab.metadata.repoSlug.includes('platform-as-a-service/infrastructure')) {
+  console.log(`MR "${danger.gitlab.mr.web_url}" review..`);
+  (async function () {
+    commonChecks();
+    await infraChecks();
   })();
 }
 
