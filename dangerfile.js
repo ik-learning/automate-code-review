d3c78cfc9bfa528e79fa4b6cf3488cf2e9cf3a1a
ivank <ivankatliarchuk@hollandandbarrett.com>
2022-10-19 20:04:45 +0100

    msk template enforcement

--- dangerfile.js
+++ dangerfile.js
@@ -1,9 +1,11 @@
 'use strict';
 // Example dangerfile. Every repo could contain this file in order to benefit from automated MR review
 const { danger, message, warn, fail, markdow } = require('danger');
-const { commonChecks, infraChecks, csvEntryAlphabeticOrderAsync, addLabels, welcomeMsg } = require(
-  process.env.IS_CI ? "/danger/lib/dangerfile.paas" : "./lib/dangerfile.paas"
-);
+const { commonChecks, infraChecks,
+  csvEntryAlphabeticOrderAsync, templateShouldBeEnforcedMsk,
+  addLabels, welcomeMsg } = require(
+    process.env.IS_CI ? "/danger/lib/dangerfile.paas" : "./lib/dangerfile.paas"
+  );
 
 // console.log(danger.gitlab.mr);
 
@@ -11,6 +13,7 @@ if (danger.gitlab.metadata.repoSlug.includes('platform-as-a-service/kafka/msk-to
   console.log(`MR "${danger.gitlab.mr.web_url}" review..`);
   (async function () {
     commonChecks();
+    await templateShouldBeEnforcedMsk();
     await csvEntryAlphabeticOrderAsync();
   })();
 }
