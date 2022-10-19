4391a692adbd59c58378cfbce192892ef0a73fae
ivank <ivankatliarchuk@hollandandbarrett.com>
2022-10-19 18:25:04 +0100

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,6 @@ set -e
 : "$DANGER_GITLAB_HOST"
 : "$TRIGGER_PAYLOAD"
 
-# export CURRENT_FOLDER="$PWD"
 cd "$WORK_DIR"
 echo "VERSION: $VERSION"
 yarn danger --version
@@ -18,5 +17,6 @@ export DANGER_TEST_PR=$(cat $TRIGGER_PAYLOAD | jq -r '.object_attributes.iid')
 export DANGER_PR_URL=$(cat $TRIGGER_PAYLOAD | jq -r '.object_attributes.url')
 
 echo "DANGER_TEST_REPO: $DANGER_TEST_REPO. DANGER_TEST_PR: $DANGER_TEST_PR. DANGER_PR_URL: $DANGER_PR_URL"
-
+echo $PWD
+ls -la
 yarn danger ci --removePreviousComments
