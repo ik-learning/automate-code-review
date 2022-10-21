61475b6f9eff77b47284d63636e59346dc888279
ivank <ivankatliarchuk@hollandandbarrett.com>
2022-10-21 16:05:37 +0100

    skip with different id

--- dangerfile.js
+++ dangerfile.js
@@ -8,28 +8,29 @@ const { commonChecks, infraChecks, skipReview,
     process.env.IS_CI ? "/danger/lib/dangerfile.paas" : "./lib/dangerfile.paas"
   );
 
-if (danger.gitlab.metadata.repoSlug.includes('platform-as-a-service/kafka/msk-topics')) {
-  console.log(`MR "${danger.gitlab.mr.web_url}" review..`);
-  (async function () {
-    commonChecks();
-    addManualApplyMessage();
-    await templateShouldBeEnforcedMsk();
-    await csvEntryAlphabeticOrder();
-  })();
-}
+if (!skipReview()) {
+  if (danger.gitlab.metadata.repoSlug.includes('platform-as-a-service/kafka/msk-topics')) {
+    console.log(`MR "${danger.gitlab.mr.web_url}" review..`);
+    (async function () {
+      commonChecks();
+      addManualApplyMessage();
+      await templateShouldBeEnforcedMsk();
+      await csvEntryAlphabeticOrder();
+    })();
+  }
 
-if (danger.gitlab.metadata.repoSlug.includes('platform-as-a-service/infrastructure')) {
-  console.log(`MR "${danger.gitlab.mr.web_url}" review..`);
-  (async function () {
-    commonChecks();
-    await infraChecks();
-  })();
-}
+  if (danger.gitlab.metadata.repoSlug.includes('platform-as-a-service/infrastructure')) {
+    console.log(`MR "${danger.gitlab.mr.web_url}" review..`);
+    (async function () {
+      commonChecks();
+      await infraChecks();
+    })();
+  }
 
-if (process.env.IS_CI) {
-  (async function () {
-    welcomeMsg({ url: process.env.CI_JOB_URL });
-    await addLabels(['review-bot']);
-  })();
+  if (process.env.IS_CI) {
+    (async function () {
+      welcomeMsg({ url: process.env.CI_JOB_URL });
+      await addLabels(['review-bot']);
+    })();
+  }
 }
-
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ set -e
 : "$DANGER_GITLAB_HOST"
 : "$TRIGGER_PAYLOAD"
 : "$CI_JOB_URL"
+: "$CI_PIPELINE_IID"
 
 cd "$WORK_DIR"
 yarn danger --version
@@ -15,12 +16,20 @@ export DANGER_FAKE_CI="YEP"
 export DANGER_TEST_REPO=$(cat $TRIGGER_PAYLOAD | jq -r '.object_attributes.target.path_with_namespace')
 export DANGER_TEST_PR=$(cat $TRIGGER_PAYLOAD | jq -r '.object_attributes.iid')
 export DANGER_PR_URL=$(cat $TRIGGER_PAYLOAD | jq -r '.object_attributes.url')
+export MR_STATE=$(cat $TRIGGER_PAYLOAD | jq -r .object_attributes.state)
+export MR_TITLE=$(cat $TRIGGER_PAYLOAD | jq -r .object_attributes.title)
 
 echo "==================================="
 echo "BUNDLE VERSION: $VERSION"
 echo "DANGER_TEST_REPO: $DANGER_TEST_REPO"
 echo "DANGER_TEST_PR: $DANGER_TEST_PR"
 echo "DANGER_PR_URL: $DANGER_PR_URL"
+echo "MR Title: ${MR_TITLE}"
+echo "MR State: ${MR_STATE}"
 echo "==================================="
 
-yarn danger ci
+if [ $MR_STATE == "opened" ] || [[ !$MR_TITLE =~ "[skip ci]" ]]; then
+  yarn danger ci --id $(uuidgen)
+else
+  echo -e "skip MR review"
+fi
