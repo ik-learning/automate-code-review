ba0d32a6c52667af8edc44f0b7033345404f2743
ivank <ivankatliarchuk@hollandandbarrett.com>
2022-11-08 18:25:39 +0000

    support fixtures locally

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,6 +17,7 @@ export DANGER_TEST_REPO=$(cat $TRIGGER_PAYLOAD | jq -r '.object_attributes.targe
 export DANGER_TEST_PR=$(cat $TRIGGER_PAYLOAD | jq -r '.object_attributes.iid')
 export DANGER_PR_URL=$(cat $TRIGGER_PAYLOAD | jq -r '.object_attributes.url')
 export MR_STATE=$(cat $TRIGGER_PAYLOAD | jq -r .object_attributes.state)
+export MR_ACTION=$(cat $TRIGGER_PAYLOAD | jq -r .object_attributes.action)
 export MR_TITLE=$(cat $TRIGGER_PAYLOAD | jq -r .object_attributes.title)
 
 echo "==================================="
@@ -26,9 +27,10 @@ echo "DANGER_TEST_PR: $DANGER_TEST_PR"
 echo "DANGER_PR_URL: $DANGER_PR_URL"
 echo "MR Title: ${MR_TITLE}"
 echo "MR State: ${MR_STATE}"
+echo "MR Action: ${MR_ACTION}"
 echo "==================================="
 
-if [ $MR_STATE == "opened" ] || [[ !$MR_TITLE =~ "[skip ci]" ]]; then
+if [ $MR_STATE == "opened" ] && [ $MR_ACTION != "approved" ] || [[ !$MR_TITLE =~ "[skip ci]" ]]; then
   # yarn danger ci --id $(uuidgen)
   yarn danger ci --removePreviousComments
 else
