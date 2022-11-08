16f7ecd1842d0fe6347e90d082fdb91bd2d20d62
ivank <ivankatliarchuk@hollandandbarrett.com>
2022-11-08 19:15:57 +0000

    skip when event is not "can_be_merged"

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,6 +17,7 @@ export DANGER_TEST_REPO=$(cat $TRIGGER_PAYLOAD | jq -r '.object_attributes.targe
 export DANGER_TEST_PR=$(cat $TRIGGER_PAYLOAD | jq -r '.object_attributes.iid')
 export DANGER_PR_URL=$(cat $TRIGGER_PAYLOAD | jq -r '.object_attributes.url')
 export MR_STATE=$(cat $TRIGGER_PAYLOAD | jq -r .object_attributes.state)
+export MR_STATUS=$(cat $TRIGGER_PAYLOAD | jq -r .object_attributes.merge_status)
 export MR_ACTION=$(cat $TRIGGER_PAYLOAD | jq -r .object_attributes.action)
 export MR_TITLE=$(cat $TRIGGER_PAYLOAD | jq -r .object_attributes.title)
 
@@ -25,12 +26,13 @@ echo "BUNDLE VERSION: $VERSION"
 echo "DANGER_TEST_REPO: $DANGER_TEST_REPO"
 echo "DANGER_TEST_PR: $DANGER_TEST_PR"
 echo "DANGER_PR_URL: $DANGER_PR_URL"
-echo "MR Title: ${MR_TITLE}"
-echo "MR State: ${MR_STATE}"
-echo "MR Action: ${MR_ACTION}"
+echo "MR Title: ${MR_TITLE}. Skip when '[skip ci]'."
+echo "MR State: ${MR_STATE}. Skip when not 'opened'."
+echo "MR Action: ${MR_ACTION}. Skip when 'approved'."
+echo "MR Merge Status: ${MR_STATUS}. Skip when not 'can_be_merged'."
 echo "==================================="
 
-if [ $MR_STATE == "opened" ] && [ $MR_ACTION != "approved" ] || [[ !$MR_TITLE =~ "[skip ci]" ]]; then
+if [ $MR_STATE == "opened" ] && [ $MR_STATUS == "can_be_merged" ] && [ $MR_ACTION != "approved" ] || [[ !$MR_TITLE =~ "[skip ci]" ]]; then
   # yarn danger ci --id $(uuidgen)
   yarn danger ci --removePreviousComments
 else
