d0baeab462dc55bad505cc75da488594987e6233
ivank <ivankatliarchuk@hollandandbarrett.com>
2022-11-09 11:09:13 +0000

    update it

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-# : "$WORK_DIR"
-# : "$DANGER_GITLAB_HOST"
+: "$WORK_DIR"
+: "$DANGER_GITLAB_HOST"
 : "$TRIGGER_PAYLOAD"
-# : "$CI_JOB_URL"
-# : "$CI_PIPELINE_IID"
+: "$CI_JOB_URL"
+: "$CI_PIPELINE_IID"
 
 cd "$WORK_DIR"
 yarn danger --version
