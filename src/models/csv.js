7f133123c411279398e7a055f9e73199515581b1
ivank <ivankatliarchuk@hollandandbarrett.com>
2023-05-05 17:20:09 +0100

    find out methods to extract
    Signed-off-by: ivank <ivankatliarchuk@hollandandbarrett.com>

--- src/models/csv.js
+++ src/models/csv.js
@@ -5,6 +5,7 @@ const { Base } = require('./base');
 
 // TODO
 // test
+// kafka move to MSK file!!!
 class CSV extends Base {
   async csvEntryAlphabeticOrder() {
     console.log('in: csvEntryAlphabeticOrder');
@@ -13,12 +14,15 @@ class CSV extends Base {
       const csvModified = csv.getKeyedPaths().modified;
       csvModified.forEach(async file => {
         const diff = await this.danger.git.diffForFile(file);
+        // extract methods
         const before = diff.before.split('\n').slice(1).filter((a) => a);
         const after = diff.after.split('\n').slice(1).filter((a) => a); // filter to remove empty elements
+        // ^
         const added = array.xor(before, after);
-        let sorted = [...after].sort((a, b) => {
-          let first = a.split(',')[0];
-          let second = b.split(',')[0];
+        // ^
+        let sorted = [...after].sort((el, nextEl) => {
+          let first = el.split(',')[0];
+          let second = nextEl.split(',')[0];
           if (first < second) {
             return -1;
           }
@@ -27,6 +31,7 @@ class CSV extends Base {
           }
           return 0;
         });
+        // ^
         // TODO: review whole msk-topics file, as it does have multiple violations
         const result = [];
         let onlyAdded = true;
@@ -34,7 +39,6 @@ class CSV extends Base {
         for (let i = 0; i < after.length; i++) {
           let first = after[i];
           let second = sorted[i];
-
           if (onlyAdded && addedAsTxt.includes(first)) {
             if (first !== second) {
               result.push(`+ ${i + 1} ${after[i]}`)
