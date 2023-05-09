e3e482c864701ddf4c0387b07be574a1e5300f10
ivank <ivankatliarchuk@hollandandbarrett.com>
2023-05-09 11:28:01 +0100

    wrap dangerFileMatch()
    Signed-off-by: ivank <ivankatliarchuk@hollandandbarrett.com>

--- tests/fixtures.js
+++ tests/fixtures.js
@@ -1,5 +1,6 @@
 
 const fs = require('fs');
+const chainsmoker = require('../node_modules/danger/distribution/commands/utils/chainsmoker.js')
 
 /**
  * Synchronously reads the entire contents of a file.
@@ -8,6 +9,18 @@ const fs = require('fs');
  */
 function setUpTestScenario(filePath) {
   return fs.readFileSync(__dirname + "/" + filePath, "utf8");
+}
+
+/**
+ * Wraper method for danger.fileMatch
+ * @param {*} keyedPaths
+ * @returns
+ */
+function dangerFileMatch(keyedPaths) {
+  if (keyedPaths) {
+    return chainsmoker.default(keyedPaths)
+  }
+  return chainsmoker.default({ modified: [], created: [], deleted: [] })
 }
 
 /**
@@ -26,4 +39,5 @@ function cleanUp(done) {
 module.exports = {
   setUpTestScenario,
   setUpTestScenarioObject,
+  dangerFileMatch,
 };
