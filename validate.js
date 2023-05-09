79d5c15905ccdec26ab69bafa063c8971d2f1ab5
ivank <ivankatliarchuk@hollandandbarrett.com>
2023-05-09 08:59:14 +0100

    added event type to validator
    Signed-off-by: ivank <ivankatliarchuk@hollandandbarrett.com>

--- validate.js
+++ validate.js
@@ -14,9 +14,14 @@ console.log(`beta version: ${process.env.BETA_VERSION}`);
 console.log("=================Validate WebHook payload====================");
 
 const webhookPayload = files.readFileSync(process.env.TRIGGER_PAYLOAD);
-const { changes  } = JSON.parse(webhookPayload);
+const { changes, user, object_kind, event_type  } = JSON.parse(webhookPayload);
 
 const isBot = (user === null || user !== null && user.username.includes('bot'))
+
+if (object_kind !== 'merge_request' && event_type !== 'merge_request') {
+  console.log('SKIP Code Review');
+  console.log(`REASON: object_kind is ${object_kind} and event_type is ${event_type}`);
+}
 
 if (isBot) {
   console.log('SKIP Code Review');
