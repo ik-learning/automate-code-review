1903751595692b11d9025e936912d0205262fb92
ivank <ivankatliarchuk@hollandandbarrett.com>
2023-05-20 20:50:34 +0100

    test validator.js
    Signed-off-by: ivank <ivankatliarchuk@hollandandbarrett.com>

--- validate.js
+++ validate.js
@@ -1,40 +1,35 @@
 'use strict';
+// Validate WebHook payload and whether or not we should run MR review
 
-// Validate WebHook payload
-import files from 'fs';
+const files = require('fs');
+const { WebHookValidator } = require('./src/validate');
 
-console.log("=================Versions====================");
+console.log("=================VERSIONS====================");
 console.log(`build version: ${process.env.BUILD_VERSION}`);
 console.log(`beta version: ${process.env.BETA_VERSION}`);
 
 console.log("=================Validate WebHook payload====================");
-
 const webhookPayload = files.readFileSync(process.env.TRIGGER_PAYLOAD);
-const { changes, user, object_kind, event_type  } = JSON.parse(webhookPayload);
-
-const isBot = (user === null || user !== null && user.username.includes('bot'))
+const target = new WebHookValidator(webhookPayload);
 
 const exitWithReason = (reason) => {
   console.log('SKIP Code Review');
   console.log(`REASON: ${reason}`);
+  console.log(target.toString())
   console.log("================validate.js FALSE==================");
   console.log("=================ABORT=================");
   process.exit(1);
 }
 
-if (object_kind !== 'merge_request' && event_type !== 'merge_request') {
-  console.log('SKIP Code Review');
-  console.log(`REASON: object_kind is ${object_kind} and event_type is ${event_type}`);
+if (target.isNotAMergeRequest()) {
+  exitWithReason(`object_kind is ${target.object_kind} and event_type is ${target.event_type}`)
 }
 
-if (isBot) {
-  console.log('SKIP Code Review');
-  console.log('REASON: BOT detected');
-  console.log(JSON.stringify(JSON.parse(webhookPayload), null, 2))
+if (target.isBot()) {
+  exitWithReason("BOT detected")
 }
 
-// This check is too restrictive. Should validate where only 'description' is updated or some other areas as well.
-if ('description' in changes) {
+if (target.isOnlyDescriptionUpdated()) {
   exitWithReason('only description updated')
 }
 console.log("================validate.js OK==================");
